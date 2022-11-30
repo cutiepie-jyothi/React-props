59db59dc8448db848a29f8d60be4278da399d4ad
Tejasri-99 <tejapamulapati99@gmail.com>
2022-11-30 16:51:36 +0530

    PropsExample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <div>
+      <PropsExample name="jyothi" age="20"/>
+      <PropsExample name="hari" age="10"/>
+      {/*<p>Good afternoon</p>
+      <h1>Heading tag</h1>
+      <img src="https://media.istockphoto.com/id/919525342/photo/taj-mahal-in-agra-india-in-the-light-of-the-full-moon.jpg?s=612x612&w=0&k=20&c=y9sk_Gwnur6R_nFzgKAJEdu3_9iJzJiFkEUfp9pxX1c=" alt="Taj mahal"/>
+      <ClassComponent/>*/}
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
