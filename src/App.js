6f1243e55dea9528aa9dec3f2775f74e9ba5172e
Tejasri-99 <tejapamulapati99@gmail.com>
2022-12-01 16:47:43 +0530

    states and events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 //import ClassComponent from './ClassComponent'
-import PropsExample from './PropsExample'
+//import PropsExample from './PropsExample'
+//import FuncProps from './FuncProps'
+import DemoExample from './DemoExample'
+import StateExample from './StateExample'
+import Events from './Events'
+
 function App(){
   return(
     <div>
+      <Events/>
+      {/*<DemoExample prize="1st prize"/>
+      <StateExample/>
       <PropsExample name="jyothi" age="20"/>
+      <FuncProps carname="rolls royce" year="2000"/>
+      <h1>Hello</h1>
       <PropsExample name="hari" age="10"/>
-      {/*<p>Good afternoon</p>
+      <p>Good afternoon</p>
       <h1>Heading tag</h1>
       <img src="https://media.istockphoto.com/id/919525342/photo/taj-mahal-in-agra-india-in-the-light-of-the-full-moon.jpg?s=612x612&w=0&k=20&c=y9sk_Gwnur6R_nFzgKAJEdu3_9iJzJiFkEUfp9pxX1c=" alt="Taj mahal"/>
       <ClassComponent/>*/}
@@ -13,3 +23,4 @@ function App(){
   )
 }
 export default App
+
